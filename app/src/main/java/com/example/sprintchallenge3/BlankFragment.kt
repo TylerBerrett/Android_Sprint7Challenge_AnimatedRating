e0eef0df05ba9b8d147f8ed2ed05835e26a5339e
Tyler Berrett <btylerberrett@gmail.com>
2019-08-23 12:05:58 -0600

    fix recyclerview fix fragment
    can add names to list with non custom view

---
+++ app/src/main/java/com/example/sprintchallenge3/BlankFragment.kt
@@ -0,0 +1,83 @@
+package com.example.sprintchallenge3
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.display_fragment.*
+import kotlinx.android.synthetic.main.display_fragment.view.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [BlankFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [BlankFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class BlankFragment : DialogFragment() {
+
+    private var name: String? = null
+    private var rating: Int? = null
+    private val code = 0
+
+
+    private var param1: String? = null
+    private var param2: String? = null
+    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.display_fragment, container, false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        add_to_list.setOnClickListener {
+            listener?.onFragmentInteraction(edit_text.text.toString(), rating_bar.rating)
+        }
+
+
+
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+
+    interface OnFragmentInteractionListener {
+        fun onFragmentInteraction(name: String, rating: Float)
+    }
+
+
+}
